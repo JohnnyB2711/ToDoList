942ca5656dd987a258ecaabb823d81398a2c110e
Евгения <eugenebeklemisheva2711@gmail.com>
2019-08-15 10:34:38 +0300

    скрыть/показать блок

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function LoadToDo() {
     const button_save = document.getElementsByName("button_save");
     const button_wrap = document.getElementById("wrap_button");
+    const button_show = document.getElementById("show_button");
     const input = document.querySelector("input[type='text']");
     const ul = document.querySelector("ul.tasks");
     const button_add = document.getElementsByName("button_add");
@@ -14,7 +15,7 @@ function LoadToDo() {
         span.classList.add("text");
         button_delete.classList.add("button_delete");
         span.append(task);
-        li.append(span,button_delete);
+        li.append(span, button_delete);
         ul.append(li);
         DeleteTask(button_delete)
     }
@@ -25,26 +26,27 @@ function LoadToDo() {
             CreateTask();
         }
     });
-    button_save.addEventListener('click', () => {
-        localStorage.setItem('tasks', ul.innerHTML)
-    })
 
-    function DeleteTask(button){
-        button.addEventListener("click",(event)=>{
+    function DeleteTask(button) {
+        button.addEventListener("click", (event) => {
             button.parentElement.remove();
             //event.stopPropagation();
         })
     }
 
-    document.getElementById('wrap_button').onclick=function WrapTasks() {
-        alert("fffffff");
-    }
-    button_wrap.click(function () {
-        alert("fffffff");
-    });
-    function ShowTasks() {
+    function Wrap_Show_Tasks(button) {
 
+            button1.addEventListener("click", (event) => {
+                const day_tasks = button.parentElement.querySelector("ul.tasks");
+               // if (day_tasks.style.display = "block")
+                    day_tasks.style.display = "none";
+               // else
+              //      day_tasks.style.display = "block"
+            })
     }
+
+
+    Wrap_Show_Tasks(button_wrap);
 }
 
 document.addEventListener("DOMContentLoaded", LoadToDo);
