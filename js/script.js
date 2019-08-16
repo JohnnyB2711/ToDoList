0341a94adbb2b113183dbab320e69179534d9540
Евгения <eugenebeklemisheva2711@gmail.com>
2019-08-16 11:07:14 +0300

    Доделать: 1.занести картинки в папку и разобраться с путями 3.подумать,нужна ли кнопка wrap/show 4.прокрутка 5.сохранение состояния при перезагрузке страницы 6.зачеркивание выполненных дел --------------------------- Теория: 1.event.target 2.операторы присваивания/сравнения

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 function LoadToDo() {
-    const button_save = document.getElementsByName("button_save");
-    const button_wrap = document.getElementById("mon_button");
+    //const button_save = document.getElementsByName("button_save");
+    //const button_wrap = document.getElementById("mon_button");
     const monday_block = document.getElementById("monday");
     const input = document.querySelector("input[type='text']");
     const button_add = document.getElementsByName("button_add");
     const day_block = document.getElementById("day_block");
     const day = document.querySelectorAll('.day_block > span.day');
+    const add_button = document.querySelector('#add_button')
     let status_active;
 
-
+    //Функция создания задачи
     function CreateTask() {
         if (status_active) {
-            //const block = document.querySelector('span.active');
             const ul = document.querySelector('span.active > ul')
             const li = document.createElement("li");
             const button_delete = document.createElement("button");
@@ -28,17 +28,25 @@ function LoadToDo() {
             alert('Выбери день недели!')
         }
     }
-
+    //Добавление по нажатию кнопки
+    add_button.addEventListener('click',(event)=>{
+            NewTask()
+    })
+    //Добавление по нажатию "Enter"
     input.addEventListener('keypress', (keyPressed) => {
         const keyEnter = 13;
         if (keyPressed.which == keyEnter) {
-            if (input.value !== '') {
-                CreateTask();
-                input.value = '';
-            } else alert("Введите задачу")
+            NewTask()
         }
     });
-
+    //Функция проверки корректности ввода задачи
+    function NewTask() {
+        if (input.value !== '') {
+            CreateTask();
+            input.value = '';
+        } else alert("Введите задачу")
+    }
+    //Функция удаления задачи
     function DeleteTask(button) {
         button.addEventListener('click', (event) => {
             button.parentElement.remove();
@@ -46,11 +54,7 @@ function LoadToDo() {
         })
     }
 
-    /*    function ActiveDay(day) {
-            day.addEventListener('click', (event) => {
-                day.classList.add("active")
-            })
-        }*///
+
     let select_day;
     day_block.onclick = function (event) {
         let day = event.target;
@@ -66,37 +70,37 @@ function LoadToDo() {
         select_day.classList.remove('active')
     }
 
-        function ActiveDay(day) {
-            if (select_day) {
-                select_day.classList.remove('active');
-                // console.log(day.classList)
-            }
-            select_day = day;
-            select_day.classList.add('active');
-            // console.log(day.classList);
+    function ActiveDay(day) {
+        if (select_day) {
+            select_day.classList.remove('active');
+            // console.log(day.classList)
         }
-
-        /*    function Wrap_Show_Tasks(button) {
-                const day_tasks = button.parentElement.querySelector("ul.task");
-                day_tasks.style.display = 'block';
-                button.style.background='url("../wrap.png") no-repeat'
-                button.addEventListener('click', (event) => {
-                    if (day_tasks.style.display == 'block')
-                    {
-                        day_tasks.style.display = 'none';
-                        button.style.background='url("../show.png") no-repeat'
-                    }
-                    else
-                    {
-                        day_tasks.style.display = 'block';
-                        button.style.background='url("../wrap.png") no-repeat'
-                    }
-
-                })
-            }*/
-
-        // Wrap_Show_Tasks(button_wrap);
-
+        select_day = day;
+        select_day.classList.add('active');
+        // console.log(day.classList);
     }
 
-    document.addEventListener("DOMContentLoaded", LoadToDo);
+    /*    function Wrap_Show_Tasks(button) {
+            const day_tasks = button.parentElement.querySelector("ul.task");
+            day_tasks.style.display = 'block';
+            button.style.background='url("../wrap.png") no-repeat'
+            button.addEventListener('click', (event) => {
+                if (day_tasks.style.display == 'block')
+                {
+                    day_tasks.style.display = 'none';
+                    button.style.background='url("../show.png") no-repeat'
+                }
+                else
+                {
+                    day_tasks.style.display = 'block';
+                    button.style.background='url("../wrap.png") no-repeat'
+                }
+
+            })
+        }*/
+
+    // Wrap_Show_Tasks(button_wrap);
+
+}
+
+document.addEventListener("DOMContentLoaded", LoadToDo);
